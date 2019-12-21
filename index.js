9cc190f77798934a7af0ea38ad374fa7f2561360
Yuzunoha <nurugamer2@gmail.com>
2019-12-22 04:19:52 +0900

    全部のapiがいけた

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const port = process.env.PORT || 3000; // port番号を指定
 
 const urlPosts = 'https://teachapi.herokuapp.com/posts';
+const urlSignUp = 'https://teachapi.herokuapp.com/sign_up';
+
 const token = 'l4hKn0Wcp5sNBgQc9MZL7Qtt';
 
 const getHeaderObj = req => {
@@ -43,7 +45,23 @@ app.get('/api/v1/get', (req, res) => {
 app.post('/api/v1/post', (req, res) => {
   const headerObj = getHeaderObj(req);
   const bodyJson = JSON.stringify(req.body);
-  return fetch(urlPosts, {
+  fetch(urlPosts, {
+    method: 'POST',
+    headers: headerObj,
+    body: bodyJson
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(responseJson => {
+      res.json(JSON.stringify(responseJson));
+    });
+});
+
+app.post('/api/v1/sign_up', (req, res) => {
+  const headerObj = getHeaderObj(req);
+  const bodyJson = JSON.stringify(req.body);
+  fetch(urlSignUp, {
     method: 'POST',
     headers: headerObj,
     body: bodyJson
