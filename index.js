d2eece48943cd2e0be06ea98edb1aa0960c7825b
Yuzunoha <nurugamer2@gmail.com>
2019-12-22 01:26:07 +0900

    headerが取り出せた

--- index.js
+++ index.js
@@ -6,19 +6,26 @@ const port = process.env.PORT || 3000; // port番号を指定
 const url = "https://teachapi.herokuapp.com/posts";
 const token = "l4hKn0Wcp5sNBgQc9MZL7Qtt";
 
+const getHeaderObj = req => {
+  const authorization = req.header("authorization");
+  return {
+    "Content-Type": "application/json",
+    Authorization: authorization
+  };
+};
+
 app.get("/api/v1/get/", function(req, res) {
   // { host: 'localhost:3000',
   // 'user-agent': 'curl/7.58.0',
   // accept: '*/*',
   // 'content-type': 'application/json',
   // authorization: 'Bearer l4hKn0Wcp5sNBgQc9MZL7Qtt' }
-  console.log(req.headers);
+  headerObj = getHeaderObj(req);
+  console.log(headerObj);
+
   fetch(url, {
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token
-    }
+    headers: headerObj
   })
     .then(res => {
       return res.json();
